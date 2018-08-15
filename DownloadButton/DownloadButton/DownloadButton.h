5ffebb93ae1570edcdd5dcde4388ca69edeee75a
Philipp Rembold <philipp@macuja.com>
2018-08-15 10:40:52 +0200

    gitignore for carthage checkout

---
+++ DownloadButton/DownloadButton/DownloadButton.h
@@ -0,0 +1,19 @@
+//
+//  DownloadButton.h
+//  DownloadButton
+//
+//  Created by Philipp Rembold on 15.08.18.
+//  Copyright © 2018 Boards & More. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DownloadButton.
+FOUNDATION_EXPORT double DownloadButtonVersionNumber;
+
+//! Project version string for DownloadButton.
+FOUNDATION_EXPORT const unsigned char DownloadButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownloadButton/PublicHeader.h>
+
+
